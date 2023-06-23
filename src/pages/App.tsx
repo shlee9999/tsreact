b3adf79212270f548a09b5d98cbcb6d4e7f834ff
shlee9999 <95556588+shlee9999@users.noreply.github.com>
2023-06-23 22:12:44 +0900

    Initial Commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,10 @@
+import { FC } from 'react'
+import { Root } from './styled'
+
+type AppProps = {
+  className?: string
+}
+
+export const App: FC<AppProps> = ({ className }) => {
+  return <Root className={className}>Helo</Root>
+}
